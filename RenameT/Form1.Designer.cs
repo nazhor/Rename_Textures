4bd5fa2e84a674c0c6e3048cf14b03c507bef73a
Nazhor <dbernad4220@gmail.com>
2019-09-04 21:13:49 +0200

    Setup. Migrate to Github

---
+++ RenameT/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace RenameT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.BT_Rename = new System.Windows.Forms.Button();
+            this.TB_Name = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BT_Rename
+            // 
+            this.BT_Rename.Location = new System.Drawing.Point(12, 54);
+            this.BT_Rename.Name = "BT_Rename";
+            this.BT_Rename.Size = new System.Drawing.Size(260, 23);
+            this.BT_Rename.TabIndex = 0;
+            this.BT_Rename.Text = "Rename";
+            this.BT_Rename.UseVisualStyleBackColor = true;
+            this.BT_Rename.Click += new System.EventHandler(this.BT_Rename_Click);
+            // 
+            // TB_Name
+            // 
+            this.TB_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TB_Name.Location = new System.Drawing.Point(13, 13);
+            this.TB_Name.Name = "TB_Name";
+            this.TB_Name.Size = new System.Drawing.Size(259, 29);
+            this.TB_Name.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(285, 89);
+            this.Controls.Add(this.TB_Name);
+            this.Controls.Add(this.BT_Rename);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Rename Textures";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button BT_Rename;
+        private System.Windows.Forms.TextBox TB_Name;
+    }
+}
+
---
+++ RenameT/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RenameT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BT_Rename_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("probando");
+            if (TB_Name.Text == "")
+                MessageBox.Show("Que no has puesto noooombre");
+            else
+            {
+                string[] fileEntries = Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory);
+                string newNameTexture = TB_Name.Text;
+
+                foreach (string fileNameOld in fileEntries)
+                {
+                    if (fileNameOld.Contains(".png"))
+                    {
+                        if ((fileNameOld.Contains("Basecolor")) ||
+                            (fileNameOld.Contains("BaseColor")))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_BaseColor.png");
+                        }
+                        else if (fileNameOld.Contains("Emissive"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_Emissive.png");
+                        }
+                        else if (fileNameOld.Contains("Normal"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_Normal.png");
+                        }
+                        else if (fileNameOld.Contains("OcclusionRoughnessMetallic"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_ORM.png");
+                        }
+                        else if (fileNameOld.Contains("Ambient_occlusion"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_AmbientOcclusion.png");
+                        }
+                        else if (fileNameOld.Contains("Metallic"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_Metallic.png");
+                        }
+                        else if (fileNameOld.Contains("Roughness"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_Roughness.png");
+                        }
+                        else if (fileNameOld.Contains("Specular"))
+                        {
+                            Renombrar(fileNameOld, newNameTexture, "_Specular.png");
+                        }
+                    }                    
+                }
+            }
+
+
+        }
+
+        //Función para renombrar un fichero
+        private static void Renombrar(string pOld, string pNew, string pType)
+        {
+            int startIndex = pOld.LastIndexOf('\\');
+            string pth = pOld.Substring(0, startIndex + 1);
+            string fileNameNew = pth + pNew + pType;
+            try
+            {
+                System.IO.File.Move(pOld, fileNameNew);
+            }
+            catch
+            {
+                string msg = "Error al renombrar la textura " + pNew + pType + " ¿Puede ser que ya exista una con ese nombre en la carpeta destino?";
+                MessageBox.Show(msg, "Algo no va bien", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+    }
+}
